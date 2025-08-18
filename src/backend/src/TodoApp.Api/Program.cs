513edae00940c594951e7c20b7ccdbdc2be1abce
Gideão Souza <gideao_souza@outlook.com>
2025-08-18 09:30:49 -0400

    Refatoração para uso de segredos e segurança de senhas
    Modifica o método `AddPostgreSqlContexts` para acesso público e leitura de senhas de arquivos secretos. Atualiza `Program.cs` e `appsettings.Development.json` para usar segredos do Docker. Altera `JwtService.cs` para ler a chave secreta JWT de um arquivo secreto. Atualiza `docker-compose.override.yml` e `docker-compose.yml` para remover senhas codificadas e usar segredos. Adiciona `SecretsConfigurationProvider.cs` para gerenciar segredos de forma segura. Vários arquivos de texto foram criados ou atualizados para armazenar senhas e chaves secretas.

--- src/backend/src/TodoApp.Api/Program.cs
+++ src/backend/src/TodoApp.Api/Program.cs
@@ -3,6 +3,16 @@ using TodoApp.Api.Middleware;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Configuration.AddDockerSecrets(
+    "ConnectionStrings__Aplicacao",
+    "ConnectionStrings__Eventos",
+    "MassTransit__RabbitMQ__Password",
+    "ConnectionStrings__AzureBlobStorage",
+    "JWT_SECRET_KEY"
+);
+
+builder.Configuration.AddEnvironmentVariables();
+
 builder.Services.AddAppServices(builder.Configuration);
 
 var app = builder.Build();
